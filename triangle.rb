3e85a5ef77769e5b3c49a35b3300ad59ce90e178
Andrew <andrew.amesbury@loyalty.co.nz>
2016-08-17 04:00:56 +1200

    control-flow with if-else

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,30 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+#equilateral first
+  if (a == b)
+    if (b == c)
+      return :equilateral
+    end
+    # then check isosceles for a == b
+    if ((b > c) || (c > b))
+      return :isosceles
+    end
+  end
+  # then check isosceles for b == c
+  if (b == c)
+    if ((a > b) || (a < c))
+      return :isosceles
+    end
+  end
+  # then check isosceles for a == c
+  if (a == c)
+    if ((a > b) || (b > a))
+      return :isosceles
+    end
+  end
+  # finally, if not caught - it's a scalene
+  return :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
